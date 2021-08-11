634921a26bbfaaf763dd2bf0ba7e9b4edae1c7c0
Ramin <rasadeghnasab@gmail.com>
2021-08-11 17:06:10 +0430

    - Rename Header to Title

--- app/StateMachines/Machines/QA/States/Stats.php
+++ app/StateMachines/Machines/QA/States/Stats.php
@@ -16,7 +16,7 @@ class Stats implements StateInterface
         $answered = $questions->whereIn('status', ['Correct', 'Incorrect'])->count();
         $correct = $questions->where('status', 'Correct')->count();
 
-        $command->titledTable(['Header', 'Value'], [
+        $command->titledTable(['Title', 'Value'], [
             ['Total', $questions->count()],
             ['Answered', sprintf('%%%s', number_format($answered * 100 / $all))],
             ['Correct', sprintf('%%%s', number_format($correct * 100 / $all))]
