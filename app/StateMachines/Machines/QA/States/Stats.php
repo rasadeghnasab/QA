5f4bc72b015928ccffc0855dab22a0a8acc64d84
Ramin <rasadeghnasab@gmail.com>
2021-08-11 20:57:24 +0430

    - Fix Stats bug

--- app/StateMachines/Machines/QA/States/Stats.php
+++ app/StateMachines/Machines/QA/States/Stats.php
@@ -12,14 +12,19 @@ class Stats implements StateInterface
     {
         $questions = $command->user()->questions()->get();
 
-        $all = $questions->count();
-        $answered = $questions->whereIn('status', ['Correct', 'Incorrect'])->count();
-        $correct = $questions->where('status', 'Correct')->count();
+        $answered = $correct = 0;
+        if ($all = $questions->count()) {
+            $answered = $questions->whereIn('status', ['Correct', 'Incorrect'])->count();
+            $answered = number_format($answered * 100 / $all);
+
+            $correct = $questions->where('status', 'Correct')->count();
+            $correct = number_format($correct * 100 / $all);
+        }
 
         $command->titledTable(['Title', 'Value'], [
-            ['Total', $questions->count()],
-            ['Answered', sprintf('%%%s', number_format($answered * 100 / $all))],
-            ['Correct', sprintf('%%%s', number_format($correct * 100 / $all))]
+            ['Total', $all],
+            ['Answered', sprintf('%%%s', $answered)],
+            ['Correct', sprintf('%%%s', $correct)]
         ],
             'Stats'
         );
